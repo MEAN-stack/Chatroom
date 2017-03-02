0718301a25c20f1ea5765014ddb9073fd903f37d
Paul Robertson <paul.robertson@simulation-systems.co.uk>
2017-03-02 17:58:23 +0000

    work on rooms

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.use(morgan('combined'))
 
 app.use(require('./controllers'))
 
+var Room = require('./models/room')
+Room.remove({}, function(err, room){})
+var room1 = new Room({"roomname" : "The Pub", "topic" : "Random Chat", "members" : []})
+room1.save(function(err, room) {})
+var room2 = new Room({"roomname" : "Node", "topic" : "node.js", "members" : [
+          "Paul Robertson",
+          "Alonzo Church",
+          "Alan Turing",
+          "Haskell Curry",
+          "Ryan Dahl"
+        ]})
+room2.save(function(err, room) {})
+var room3 = new Room({"roomname" : "Java", "topic" : "Java Talk", "members" : []})
+room3.save(function(err, room) {})
+
 app.listen(3000, function() {
   console.log('Chatroom server listening on port ', 3000)
 })
