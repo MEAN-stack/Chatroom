c091f530e2d7e301ba3470eff2b64bc73f5609e4
Paul Robertson <paul.robertson@simulation-systems.co.uk>
2017-03-06 13:35:04 +0000

    Debugging user login

--- controllers/api/sessions.js
+++ controllers/api/sessions.js
@@ -5,9 +5,12 @@ var jwt = require('jwt-simple')
 var config = require('../../config')
 
 router.post('/', function(req, res, next) {
+  console.log("creating session for "+req.body.username)
   User.findOne({username: req.body.username})
   .select('password')
-//  .select('username')
+  .select('username')
+  .select('email')
+  .select('fullName')
   .exec(function(err, user) {
     if (err) {
       return next(err)
@@ -17,17 +20,23 @@ router.post('/', function(req, res, next) {
     }
     bcrypt.compare(req.body.password, user.password, function(err, valid) {
       if (err) {
+        console.log(err.message)
         return next(err)
       }
       if (!valid) {
         return res.sendStatus(401)
       }
-      var token = jwt.encode({
-        userrname: user.username,
-        fullName: user.fullName,
-        email: user.email
-      }, config.secret)
-      res.send(token)
+      try {
+        var token = jwt.encode({
+          userrname: user.username,
+          fullName: user.fullName,
+          email: user.email
+        }, config.secret)
+        res.send(token)
+      }
+      catch (ex) {
+        console.log('Caught '+ex.message)
+      }
     })
   })
 })
--- ng/routes.js
+++ ng/routes.js
@@ -2,6 +2,6 @@ angular.module('app')
 .config(function($routeProvider) {
   $routeProvider
   .when('/',         {controller: 'RoomsCtrl',   templateUrl: 'home.html'})
-  .when('/register', {controller: RegisterCtrl, templateUrl: register.html})
-  .when('/login',    {controller: LoginCtrl,    templateUrl: login.html})
+//  .when('/register', {controller: 'RegisterCtrl', templateUrl: 'register.html'})
+  .when('/login',    {controller: 'LoginCtrl',    templateUrl: 'login.html'})
 })
