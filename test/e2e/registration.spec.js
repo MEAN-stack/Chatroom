649004e409ee1b18caa86cf7f0a2f0a44bfbdf19
Paul Robertson <paul.robertson@simulation-systems.co.uk>
2017-03-09 18:00:13 +0000

    protractor testing

--- test/e2e/registration.spec.js
+++ test/e2e/registration.spec.js
@@ -2,8 +2,19 @@ describe('User registration', function() {
   it('registers a new user', function() {
     // go to homepage
     browser.get('http://localhost:3001')
-    // click 'Register'
-    // fill out form and submit
+    
+    // click 'register'
+    element(by.css('nav .register')).click()
+    
+    element(by.model('username')).sendKeys('Lambda')
+    element(by.model('fullName')).sendKeys('Alonzo Church')
+    element(by.model('email')).sendKeys('Lambda@calculus.com')
+    element(by.model('password')).sendKeys('passme')
+    element(by.css('form .btn')).click()
+
+    browser.pause()
+
+    
 
     // the user should see a confirmation message
   })
